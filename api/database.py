037851ebd088e1ddcd037123ebcd0f601585e988
Geo Bibas <denverparole@gmail.com>
2025-01-20 03:48:45 +0800

    added transaction/statement timeout set for 60 seconds

--- api/database.py
+++ api/database.py
@@ -4,9 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 URL_DATABASE = "postgresql://postgres:password@localhost:5432/pharmassist"
 
-engine = create_engine(URL_DATABASE)
-
-
+engine = create_engine(
+    URL_DATABASE,
+    pool_recycle=3600,  # Recycle connections after 1 hour
+    pool_pre_ping=True,  # Validates connections before using them
+    connect_args={
+        "options": "-c statement_timeout=60000"  # 60-second statement timeout
+    }
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
